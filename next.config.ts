249e645910b29d7afbfa7f9748d2f6bdf2a31f25
anildani36 <anildani36@gmail.com>
2025-12-05 15:07:35 +0530

    beta test push to prod

---
+++ next.config.ts
@@ -0,0 +1,25 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.jsdelivr.net',
+        pathname: '/gh/devicons/devicon/icons/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.icons8.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'zod.dev',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
